ccaddfd0b93ab87c31adcd95ecc507a6ae61b07a
Angelo Zambrano <angelozam17@gmail.com>
2024-01-01 17:40:07 -0500

    :heavy_plus_sign: Feat: use clap

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,33 @@
-static TEXT: &str = "\
-Every face, every shop,
-bedroom window, public-house, and
-dark square is a picture
-feverishly turned--in search of what?
-It is the same with books.
-What do we seek
-through millions of pages?";
+use clap::Parser;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader, Error},
+};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short = 'p', long = "pattern")]
+    pattern: String,
+
+    #[arg(short = 'f', long = "file")]
+    path: String,
+}
 
 fn main() {
-    let needle = "is";
-    let mut tags: Vec<usize> = vec![];
-    let ctx_lines = 1;
-    let mut ctx: Vec<Vec<(usize, String)>> = vec![];
+    let args = Args::parse();
+    let reader = process_file(&args.path).expect("Something went wrong by processing file.");
 
-    for (i, line) in TEXT.lines().enumerate() {
-        if line.contains(needle) {
-            tags.push(i)
+    for (i, line) in reader.lines().enumerate() {
+        let line = line.unwrap();
+        if line.to_lowercase().contains(&args.pattern.to_lowercase()) {
+            println!("{}. {}", i + 1, line);
         }
     }
+}
 
-    if tags.is_empty() {
-        return;
-    }
-
-    for (i, line) in TEXT.lines().enumerate() {
-        for (j, tag) in tags.iter().enumerate() {
-            let lower_bound = tag.saturating_sub(ctx_lines);
-            let upper_bound = tag + ctx_lines;
-
-            if (i >= lower_bound) && (i <= upper_bound) {
-                let lines_as_string = String::from(line);
-                let local_ctx = (i, lines_as_string);
-
-                match ctx.get_mut(j) {
-                    Some(c) => c.push(local_ctx),
-                    None => ctx.push(vec![local_ctx]),
-                }
-            }
-        }
-    }
-
-    for local_ctx in ctx.iter() {
-        for (i, line) in local_ctx.iter() {
-            let line_num = i + 1;
-            println!("{}. {}", line_num, line);
-        }
-        println!("\n");
-    }
+fn process_file(path: &str) -> Result<BufReader<File>, Error> {
+    let f = File::open(path)?;
+    let reader = BufReader::new(f);
+    Ok(reader)
 }
